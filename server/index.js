ae77db8df0803aad7f9a68b6ad33ce0d6def1f93
ManuDeveloper <manuepeva@gmail.com>
2020-06-30 16:49:32 -0500

    Using React Hooks in order to get user information and send it to the backend

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const {añadirUsuario, removerUsuario, getUsuario, getUsuariosRooms} = require('./usuarios');
+
 const PORT = process.env.PORT || 2600
 
 const router = require('./router');
@@ -11,7 +13,28 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection!!!');
+    socket.on('join', ({name, room}, callback) => {
+        const {error, usuario} = añadirUsuario({id: socket.id, name, room});
+
+            socket.emit('message', {usuario: 'admin', text: `${usuario.name}, welcome to the room ${usuario.room}`});
+
+            socket.broadcast.to(usuario.room).emit('message', {usuario: 'admin', text: `${usuario.name}, has joined!`})
+
+        
+                if(error) return callback(error);
+
+                socket.join(usuario.room);
+
+                callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+                const usuario = getUsuario(socket.id);
+
+                io.to(usuario.room).emit('message', {usuario: usuario.name, text: message});
+
+                callback();
+    })
 
     socket.on('disconnect', () => {
         console.log('User had left.');
---
+++ server/usuarios.js
@@ -0,0 +1,33 @@
+const usuarios = [];
+
+const añadirUsuario = ({id, name, room}) => {
+        name = name.trim().toLowerCase();
+        room = room.trim().toLowerCase();
+
+        const usuarioExistente = usuarios.find((usuario) => usuario.room === room && usuario.name === name);
+
+        if(usuarioExistente){
+            return {error: 'Nombre de usuario en uso'};
+        }
+
+        const usuario = {id, name, room};
+
+        usuarios.push(usuario);
+
+        return {usuario}
+}
+
+const removerUsuario = (id) => {
+        const index = usuarios.findIndex((usuario) => usuario.id === id);
+        if(index !== -1){
+            return usuarios.splice(index, 1)[0];
+        }
+
+}
+const getUsuario = (id) => {
+        usuarios.find((usuario) => usuario.id === id);
+}
+
+const getUsuariosRooms = (room) => usuarios.filter((usuario) => usuario.room === room);
+
+module.exports = {añadirUsuario, removerUsuario, getUsuario, getUsuariosRooms};
