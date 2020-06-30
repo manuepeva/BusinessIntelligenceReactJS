bba86ebd95e43d21b4ea4c820226682d7c1a65e6
ManuDeveloper <manuepeva@gmail.com>
2020-06-30 15:25:41 -0500

    Using React Hooks in order to get user information and send it to the backend

--- server/index.js
+++ server/index.js
@@ -12,8 +12,12 @@ const io = socketio(server);
 
 io.on('connection', (socket) => {
     console.log('We have a new connection!!!');
+
+    socket.on('disconnect', () => {
+        console.log('User had left.');
+    })
 });
 
 app.use(router);
 
-server.listen(PORT, () => console.log('server has started on Port ${PORT}'));
+server.listen(PORT, () => console.log(`server has started on Port ${PORT}`));
