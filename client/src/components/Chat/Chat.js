dbf3e53003a8fa7a6a5fd75e58277031234f1f62
ManuDeveloper <manuepeva@gmail.com>
2020-07-10 10:38:57 -0500

    checking out socket.io connectivity

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -56,7 +56,7 @@ const Chat = ({location}) => {
     return (
         <div className="containerExterior">
             <div className="container">
-                {/* <InfoBar room={room}/> */}
+                <InfoBar room={room}/>
               <Messages messages={messages} name={name}/>
                 <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
             </div>
--- server/Router.js
+++ server/Router.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
 const router = express.Router();
 
 router.get('/', function (req, res, next){
-    res.json({msg:'Server is up and running'});
+    console.log(res.json({msg:'Server is up and running'}))
+   
 });
 
 module.exports = router;
